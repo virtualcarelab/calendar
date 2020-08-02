cb7e312d62df3d833d3344d6b444d81f43a51174
leils <e.leiachang@gmail.com>
2020-08-01 17:20:47 -0700

    add gcal source

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react'
+import './App.css';
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import googleCalendarPlugin from '@fullcalendar/google-calendar'
+
+const apiKey = "AIzaSyBIwJdxiiUocK4yJLF6qLWxkc44-_7tf_0";
+const gCalId = "cns2ghia12k0nhtqgr6sn50aic@group.calendar.google.com";
 
 export default class App extends React.Component {
 
   render() {
     return (
       <FullCalendar
-        plugins={[ dayGridPlugin, interactionPlugin ]}
-        dateClick={this.handleDateClick}
+        plugins={[ dayGridPlugin, interactionPlugin, googleCalendarPlugin ]}
+        googleCalendarApiKey={apiKey}
+        events={{googleCalendarId: gCalId}}
+        height="auto"
       />
     )
-  }
-
-  handleDateClick = (arg) => { // bind with an arrow function
-    alert(arg.dateStr)
   }
 }
